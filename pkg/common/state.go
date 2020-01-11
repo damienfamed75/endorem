7bd1035977af6675f5d4e3249c49f474862627a8
Damien Stamates <sta8071@s310925.nml.com>
2020-01-11 03:06:33 -0600

    Refine player attacking hitbox

--- pkg/common/state.go
+++ pkg/common/state.go
@@ -1,12 +1,17 @@
 package common
 
+//go:generate stringer -type=State -linecomment -output=state_string.go
+
+type State uint8
+
+// Do not edit comments.
 const (
-	StateIdle    = "idle"
-	StateLeft    = "left"
-	StateRight   = "right"
-	StateJumping = "jumping"
-	StateFalling = "falling"
-	StateAttack  = "attack"
-	StateCrouch  = "crouch"
-	StateDead    = "dead"
+	StateIdle    State = iota + 1 // idle
+	StateLeft                     // left
+	StateRight                    // right
+	StateJumping                  // jumping
+	StateFalling                  // falling
+	StateAttack                   // attack
+	StateCrouch                   // crouch
+	StateDead                     // dead
 )
