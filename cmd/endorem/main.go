d4dd5ce713f6f0668305b4b4691dd94e759f87b4
Damien Stamates <sta8071@s310925.nml.com>
2020-01-10 18:41:52 -0600

    plane: create testing plane & testing package

--- cmd/endorem/main.go
+++ cmd/endorem/main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"github.com/damienfamed75/endorem/pkg/testing"
 	r "github.com/lachee/raylib-goplus/raylib"
 )
 
 func main() {
 	r.InitWindow(800, 600, "Endorem")
+	defer r.CloseWindow()
+
+	tPlane := testing.NewPlane()
 
 	for !r.WindowShouldClose() {
 		r.BeginDrawing()
-		r.ClearBackground(r.RayWhite)
+		r.ClearBackground(r.Black)
 
 		r.DrawText("Endorem hello", 20, 20, 40, r.GopherBlue)
 
+		tPlane.Draw()
+		
 		r.EndDrawing()
 	}
-
-	defer r.CloseWindow()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/damienfamed75/endorem
 
 go 1.13
 
-require github.com/lachee/raylib-goplus v0.0.0-20200106135705-aaff89088b9f
+require (
+	github.com/SolarLune/resolv v0.0.0-20190821203317-2f6176d8d107
+	github.com/gen2brain/raylib-go v0.0.0-20191203131114-468adaa7ecc9 // indirect
+	github.com/lachee/raylib-goplus v0.0.0-20200106135705-aaff89088b9f
+	github.com/solarlune/resolv v0.0.0-20190821203317-2f6176d8d107
+	github.com/veandco/go-sdl2 v0.3.3 // indirect
+)
