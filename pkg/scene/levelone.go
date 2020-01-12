49e7035c7985c359d36e80abcfc5ff097ccfe7e1
Damien Stamates <sta8071@s310925.nml.com>
2020-01-11 23:46:15 -0600

    Clean up level one

--- pkg/scene/levelone.go
+++ pkg/scene/levelone.go
@@ -1,8 +1,6 @@
 package scene
 
 import (
-	"fmt"
-
 	"github.com/SolarLune/dngn"
 	"github.com/SolarLune/resolv/resolv"
 	"github.com/damienfamed75/endorem/pkg/common"
@@ -48,16 +46,10 @@ func (l *LevelOne) Preload() {
 
 	_, _ = x, y
 
-	fmt.Println("SPAWNX:", x)
+	// fmt.Println("SPAWNX:", x)
 	// l.player = player.NewPlayer(x, y, func() {}, l.ground, resolv.NewSpace())
 	l.player = player.NewPlayer(x, y, func() {}, l.ground, resolv.NewSpace())
 	l.camera = common.NewEndoCamera(l.player.Collision)
-	// l.camera.Zoom = 1
-	// vv := r.GetScreenToWorld2D(r.NewVector2(float32(l.player.Collision.X), float32(l.player.Collision.Y)), l.camera.Camera2D)
-	// l.camera.Offset = vv.Divide(2)
-	// l.camera.Offset.X /= 4
-	// l.camera.Offset.X /= 8
-	// l.camera.Zoom = 1.0
 
 	l.mapData.Select().By(func(x, y int) bool {
 		switch l.mapData.Get(x, y) {
@@ -115,14 +107,16 @@ func (l *LevelOne) Update(dt float32) {
 }
 
 func (l *LevelOne) Draw() {
-	r.BeginMode2D(l.overviewCam)
-	// r.BeginMode2D(l.camera.Camera2D)
+	// r.BeginMode2D(l.overviewCam)
+	r.BeginMode2D(l.camera.Camera2D)
 	r.ClearBackground(r.Black)
 
 	for i := range *l.ground {
 		(*l.ground)[i].(Drawer).Draw()
 	}
 	l.player.Draw()
+	r.DrawCircleLines(int(l.camera.Target.X), int(l.camera.Target.Y), 50, r.Red)
+	// r.DrawCircle(int(l.camera.Offset.X), int(l.camera.Offset.Y), 50, r.Red)
 
 	r.EndMode2D()
 }
