08e63a59b7dee1dec1045a72c8a3523a1b219665
damienfamed75 <thedoubledimensions@gmail.com>
2020-01-15 02:20:08 -0600

    Expanded Shape for easier raylib integration

--- pkg/physics/rectangle.go
+++ pkg/physics/rectangle.go
@@ -6,6 +6,7 @@ var _ Shape = &Rectangle{}
 
 type Rectangle struct {
 	tags []string
+	Data interface{}
 
 	r.Rectangle
 }
@@ -14,6 +15,18 @@ func NewRectangle(x, y, w, h float32) *Rectangle {
 	return &Rectangle{
 		Rectangle: r.NewRectangle(x, y, w, h),
 	}
+}
+
+func (r *Rectangle) SetData(dat interface{}) {
+	r.Data = dat
+}
+
+func (r *Rectangle) GetData() interface{} {
+	return r.Data
+}
+
+func (r *Rectangle) GetTags() []string {
+	return r.tags
 }
 
 func (r *Rectangle) HasTags(tags ...string) bool {
@@ -38,6 +51,16 @@ func (r *Rectangle) HasTags(tags ...string) bool {
 
 func (r *Rectangle) AddTags(tags ...string) {
 	r.tags = append(r.tags, tags...)
+}
+
+func (r *Rectangle) RemoveTags(tags ...string) {
+	for _, t := range tags {
+		for i := len(r.tags) - 1; i >= 0; i-- {
+			if t == r.tags[i] {
+				r.tags = append(r.tags[:i], r.tags[i+1:]...)
+			}
+		}
+	}
 }
 
 func (r *Rectangle) ClearTags() {
--- pkg/physics/shape.go
+++ pkg/physics/shape.go
@@ -3,12 +3,23 @@ package physics
 import r "github.com/lachee/raylib-goplus/raylib"
 
 type Shape interface {
+	GetData() interface{}
+	SetData(dat interface{})
+	GetTags() []string
 	HasTags(tags ...string) bool
 	AddTags(tags ...string)
+	RemoveTags(tags ...string)
 	ClearTags()
 
 	// raylib api
-	Move(x, y float32) r.Rectangle
+	MaxPosition() r.Vector2
+	MinPosition() r.Vector2
+	Position() r.Vector2
+	Center() r.Vector2
+	Size() r.Vector2
 	Overlaps(r.Rectangle) bool
 	GetOverlapRec(r.Rectangle) r.Rectangle
+	LerpPosition(pos r.Vector2, amount float32) r.Rectangle
+	SetPosition(r.Vector2) r.Rectangle
+	Move(x, y float32) r.Rectangle
 }
